2afdcf4b103faa3a43911b5b538b92a30979b98c
Sócrates Díaz <sdsxd1800@gmail.com>
2024-05-16 21:46:43 -0400

    feat: add scoring system.

--- game.gd
+++ game.gd
@@ -59,12 +59,20 @@ func calculate_score() -> int:
 		if die_value:
 			dice_matches[die_value] += 1
 	print(dice_matches)
+	
+	# DEBUG: Verify dice matches sum is six
 	var sum = 0
 	for key in dice_matches:
 		sum += dice_matches[key]
 	if sum != 6:
 		print("wrong outcome!")
-	return 0
+	# DEBUG_END
+
+	var n_of_a_kind = _n_of_a_kind(dice_matches)
+	var other_scores = _collective_score_kinds(dice_matches)
+	var total_score = [n_of_a_kind, other_scores].max()
+	print(total_score)
+	return total_score
 
 func _on_die_stopped():
 	stopped_dice += 1
@@ -75,3 +83,53 @@ func _on_die_collided(collided_with: Node, die: Die):
 	if collided_with == floor:
 		die.thrown = true
 		die.body_entered.disconnect(_on_die_collided.bind(die))
+
+func _n_of_a_kind(dice_dict: Dictionary) -> int:
+	var double_trip_counter = 0
+	var double_trip_score = 0
+	for key in dice_dict:
+		var value = dice_dict[key]
+		match [key, value]:
+			[1, var n] when n >= 3:
+				return 1000*(2**(n-3))
+			[var m, var n] when n >= 3:
+				var score = 100*m*(n-2)
+				# Handle Double Trips when 2 sets of a 3 of a kind are hit.
+				if n == 3:
+					print("3 of a kind!")
+					double_trip_score += score
+					double_trip_counter += 1
+				if double_trip_counter == 2:
+					return (score + double_trip_score) * double_trip_counter
+				return score
+			_: pass
+	return 0
+
+func _collective_score_kinds(dice_dict: Dictionary) -> int:
+	var non_zero_matches = []
+	for die in dice_dict.values():
+		if die: 
+			non_zero_matches.push_back(die)
+	
+	non_zero_matches.sort()
+	
+	match non_zero_matches:
+		[1, 1, 1, 1, 1, 1]: 
+			print("Straight!")
+			return 1500 # Straight 1-6
+		[2, 2, 2]: 
+			print("Three Pairs!")
+			return 1000 # Three Pairs
+		_: pass
+	
+	var has_two = non_zero_matches.any(func(n): n == 2)
+	var has_three = non_zero_matches.any(func(n): n == 3)
+	if has_two and has_three: return 1500 # Full House
+	
+	var singles = 0
+	if dice_dict[1] > 0:
+		singles += dice_dict[1] * 100 # Single 1's
+	if dice_dict[5] > 0:
+		singles += dice_dict[5] * 50 # Single 5's
+	
+	return singles

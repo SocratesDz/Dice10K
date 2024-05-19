1aee09bbf1d40fbf929f6f05b30b62d08a3fd78f
Sócrates Díaz <sdsxd1800@gmail.com>
2024-05-18 23:29:41 -0400

    feat: Add sfx sound for scoring points. CPU player throws dice from across the board.

--- game.gd
+++ game.gd
@@ -1,28 +1,36 @@
 class_name Game
 extends Node3D
 
-@onready var throw_point: Marker3D = $ThrowPoint
+@onready var player_throw_point: Marker3D = $PlayerThrowPoint
+@onready var cpu_throw_point: Marker3D = $CPUThrowPoint
 @onready var floor_body: StaticBody3D = $Board/Body
 @onready var throw_button: Button = %ThrowButton
-@onready var score_label: Label = %ScoreLabel
+@onready var player_score_label: Label = %PlayerScoreLabel
+@onready var cpu_score_label: Label = %CPUScoreLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var hud: Control = $HUD
 @onready var play_again_button: Button = %PlayAgainButton
+@onready var score_sfx_audio_player: AudioStreamPlayer = $ScoreSfxAudioPlayer
 
-const IMPULSE_FACTOR = 0.5
+const IMPULSE_FACTOR = 1.2
 const TORQUE_FACTOR = 0.01
 const WINNING_SCORE = 10000
+const CPU_TURN_DURATION = 2.0
 
 signal all_dice_stopped
 signal player_wins
 signal player_loses
 signal restart_game
 
-var stopped_dice = 0
+enum TurnType {
+	PLAYER, CPU
+}
+
 var dice: Array[Die] = []
-var turn_start = false
 var player_score = 0
 var cpu_score = 0
+var current_turn = TurnType.PLAYER
+var _stopped_dice = 0
 
 
 func _ready():
@@ -33,7 +41,6 @@ func _ready():
 	# Connect dice signals
 	for die in dice:
 		die.sleeping_state_changed.connect(_on_die_stopped.bind(die))
-		#die.stopped.connect(_on_die_stopped)
 	
 	# Complete turn when all dice have stopped
 	all_dice_stopped.connect(_complete_turn)
@@ -48,9 +55,8 @@ func _ready():
 	# Restart game
 	play_again_button.pressed.connect(_restart_game)
 	
-	score_label.text = str(player_score)
-	
-	turn_start = true
+	player_score_label.text = _format_score(player_score, TurnType.PLAYER)
+	cpu_score_label.text = _format_score(cpu_score, TurnType.CPU)
 
 
 func _show_end_game_ui(player_won: bool):
@@ -69,7 +75,6 @@ func _restart_game():
 
 
 func _throw_dice_action():
-	turn_start = false
 	_reset_dice()
 	_hide_throw_button()
 	_throw_dice()
@@ -77,7 +82,7 @@ func _throw_dice_action():
 
 ## Reset game state
 func _reset_dice():
-	stopped_dice = 0
+	_stopped_dice = 0
 	for die in dice:
 		die.thrown = false
 
@@ -91,41 +96,65 @@ func _show_throw_button():
 
 
 func _complete_turn():
-	player_score += calculate_score()
-	score_label.text = str(player_score)
+	var score = calculate_score()
+	if current_turn == TurnType.PLAYER:
+		player_score += score
+		player_score_label.text = _format_score(player_score, current_turn)
+	else:
+		cpu_score += score
+		cpu_score_label.text = _format_score(cpu_score, current_turn)
+	score_sfx_audio_player.play()
+	
 	if player_score >= WINNING_SCORE:
 		player_wins.emit()
 	elif cpu_score >= WINNING_SCORE:
 		player_loses.emit()
 	else:
-		_show_throw_button()
-		turn_start = true
+		if current_turn == TurnType.PLAYER:
+			current_turn = TurnType.CPU
+		else:
+			current_turn = TurnType.PLAYER
+		
+		if current_turn == TurnType.PLAYER:
+			_show_throw_button()
+		else:
+			await get_tree().create_timer(CPU_TURN_DURATION).timeout
+			_throw_dice_action()
 
 
 func _throw_dice():
 	# Set dice around marker throw point
 	for i in dice.size():
 		var die = dice[i]
+		var turn_direction_modifier = 0
+		var dice_target_position = Vector3.ZERO
+		match current_turn:
+			TurnType.PLAYER:
+				turn_direction_modifier = 1
+				dice_target_position = player_throw_point.position
+			TurnType.CPU:
+				turn_direction_modifier = -1
+				dice_target_position = cpu_throw_point.position
 		# Position dice horizontally in linear fashion
 		var margin_x = 0.20
-		die.position = throw_point.position + \
+		die.position = dice_target_position + \
 			((i - dice.size()/2) * Vector3(margin_x, 0, 0))
 		die.rotation = Vector3.ZERO
 		
 		die.set_linear_velocity(Vector3.ZERO)
 		# Apply impulse forward
-		die.apply_impulse(Vector3.FORWARD * IMPULSE_FACTOR)
+		die.apply_impulse(Vector3.FORWARD * IMPULSE_FACTOR * turn_direction_modifier)
 		# Apply torque impulse
-		die.apply_torque_impulse(Vector3.FORWARD * TORQUE_FACTOR)
+		die.apply_torque_impulse(Vector3.FORWARD * TORQUE_FACTOR * turn_direction_modifier)
 		# Detect when die hits floor
 		die.body_entered.connect(_on_die_collided.bind(die))
 
 
 func _on_die_stopped(die: Die):
 	if die.thrown and die.sleeping:
-		stopped_dice += 1
+		_stopped_dice += 1
 
-	if stopped_dice >= dice.size():
+	if _stopped_dice >= dice.size():
 		all_dice_stopped.emit()
 
 
@@ -133,6 +162,13 @@ func _on_die_collided(collided_with: Node, die: Die):
 	if collided_with == floor_body:
 		die.thrown = true
 		die.body_entered.disconnect(_on_die_collided.bind(die))
+
+
+func _format_score(score: int, turn_type: TurnType) -> String:
+	if turn_type == TurnType.PLAYER:
+		return "PLAYER\n%d" % [score]
+	else:
+		return "%d\nCPU" % [score]
 
 
 func calculate_score() -> int:
@@ -185,10 +221,10 @@ func _collective_score_kinds(dice_dict: Dictionary) -> int:
 	non_zero_matches.sort()
 	
 	match non_zero_matches:
-		[1, 1, 1, 1, 1, 1]: 
+		[1, 1, 1, 1, 1, 1]:
 			print("Straight!")
 			return 1500 # Straight 1-6
-		[2, 2, 2]: 
+		[2, 2, 2]:
 			print("Three Pairs!")
 			return 1000 # Three Pairs
 		_: pass
